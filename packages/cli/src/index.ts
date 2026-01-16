34acde86d87d1976986d12f89fe60b7cd89068ad
Võ Hoàng Đức Khoa <duckhoa.dev@gmail.com>
2026-01-16 22:31:22 +0700

    feat: [US-013] - Create CLI package structure

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,44 @@
 #!/usr/bin/env node
-// Figma Sentinel CLI - Entry point
-// Commands will be added in subsequent stories
 
-console.log('Figma Sentinel CLI');
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('figma-sentinel')
+  .description('Figma Sentinel - Design change tracking and automated sync')
+  .version('1.0.0');
+
+program
+  .command('sync')
+  .description('Sync Figma designs and detect changes')
+  .option('--dry-run', 'Preview changes without writing files')
+  .option('--cwd <dir>', 'Set working directory')
+  .option('--config <path>', 'Path to config file')
+  .action(() => {
+    console.log('sync command - to be implemented in US-014');
+  });
+
+program
+  .command('check')
+  .description('Validate setup without making changes')
+  .action(() => {
+    console.log('check command - to be implemented in US-015');
+  });
+
+program
+  .command('diff <node-id>')
+  .description('Debug specific node changes')
+  .option('--file-key <key>', 'Figma file key')
+  .action(() => {
+    console.log('diff command - to be implemented in US-016');
+  });
+
+program
+  .command('init')
+  .description('Initialize Figma Sentinel in your project')
+  .action(() => {
+    console.log('init command - to be implemented in US-017');
+  });
+
+program.parse();
